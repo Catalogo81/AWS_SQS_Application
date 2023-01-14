febd2981346d877ed7ae1e7e6dbaa0048716a086
Catalogo81 <peacematjato@gmail.com@gmail.com>
2023-01-14 18:31:53 +0200

    SQS setup and Dependencies added

---
+++ src/test/java/com/example/SQSSpringBoot/SqsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SQSSpringBoot;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+class SqsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
